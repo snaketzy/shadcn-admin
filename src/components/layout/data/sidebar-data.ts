02c8b69272a76e8cb511473fe101466e197629c3
Jerry.Tang <snaketzy@gmail.com>
2026-08-05 02:00:57 +0800

    feat: 删除Clerk模块

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -22,7 +22,6 @@ import {
   GalleryVerticalEnd,
   BookOpen,
 } from 'lucide-react'
-import { ClerkLogo } from '@/assets/clerk-logo'
 import { type SidebarData } from '../types'
 
 export const sidebarData: SidebarData = {
@@ -76,24 +75,6 @@ export const sidebarData: SidebarData = {
           title: '字典',
           url: '/dictionaries',
           icon: BookOpen,
-        },
-        {
-          title: 'Secured by Clerk',
-          icon: ClerkLogo,
-          items: [
-            {
-              title: 'Sign In',
-              url: '/clerk/sign-in',
-            },
-            {
-              title: 'Sign Up',
-              url: '/clerk/sign-up',
-            },
-            {
-              title: 'User Management',
-              url: '/clerk/user-management',
-            },
-          ],
         },
       ],
     },
