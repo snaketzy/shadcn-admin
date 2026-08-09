815f3c7064d9fad734eab1de1cd40c21f47f5d81
Jerry.Tang <snaketzy@gmail.com>
2026-08-09 11:51:31 +0800

    ui: 修改toast位置到top-right

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRouteWithContext<{
       <>
         <NavigationProgress />
         <Outlet />
-        <Toaster duration={5000} />
+        <Toaster duration={5000} position='top-right' />
         {import.meta.env.MODE === 'development' && (
           <>
             <ReactQueryDevtools buttonPosition='bottom-left' />
