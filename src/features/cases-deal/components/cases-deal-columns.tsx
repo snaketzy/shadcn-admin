7fc82ff11c7f4dc1fdddf46656ec964b0aab9f78
Jerry.Tang <snaketzy@gmail.com>
2026-09-26 22:03:03 +0800

    feat: 修复案件速览的bug

--- src/features/cases-deal/components/cases-deal-columns.tsx
+++ src/features/cases-deal/components/cases-deal-columns.tsx
@@ -969,8 +969,12 @@ export function getCasesDealColumns(params?: {
                         s(rowData.case_inquiry_type)
                     )}
                     {kvRow(
+                      '询价日期',
+                      formatDateAsHyphen(rowData.case_inquiry_date)
+                    )}
+                    {kvRow(
                       '跟进日期',
-                      formatDateAsHyphen(rowData.case_follow_date)
+                      formatDateAsHyphen(rowData.case_uptodate_date)
                     )}
                     {kvRow(
                       '船东联系人',
